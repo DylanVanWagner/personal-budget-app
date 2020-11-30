383e4a1850b35f3a829c1c735a399cf380ed5a90
Dylan Van Wagner <dvanwagn@uncc.edu>
2020-11-30 16:27:59 -0500

    Update

--- Dashboard/Dashboard.js
+++ Dashboard/Dashboard.js
@@ -1,102 +1,81 @@
-let incomesChart = document.getElementById('incomeChart').getContext('2d');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const budgetModel = require("./models/budget_schema");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
 
-Chart.defaults.global.defaultFontColor = '#111';
-Chart.defaults.global.defaultFontFamily = "'Lora', sans-serif";
+let url = 'mongodb://localhost:27017/mongo_nodejs';
 
-let incomeChart = new Chart(incomesChart, {
-    type: 'pie',
-    data: {
-        labels: ['Salary', 'Bank interest', 'Sales', 'Other'],
-        datasets: [{
-            label: 'Incomes',
-            data: [3000.00, 12.64, 120.00, 250.00],
-            backgroundColor: [
-                'rgb(0, 102, 153)',
-				'rgb(0, 255, 204)',
-                'rgb(0, 204, 102)',
-                'rgb(0, 153, 0)'
-            ],
-            borderColor: [
-                'rgb(0, 68, 102)',
-                'rgb(0, 204, 163)',
-                'rgb(0, 179, 89)',
-                'rgb(0, 128, 0)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        title:{
-			display: true,
-			text: 'Summary of your incomes by category',
-			fontSize: 18,
-			fontStyle: 'normal'
-		},
-		legend:{
-			display: true,
-			position: 'right',
-		},
-		layout:{
-			padding:{
-				top: 30
-			}
-		},
-    }
+app.use(cors());
+
+app.use('/', express.static('public'));
+
+app.get("/mongo_nodejs", (req, res) => {
+  mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+            console.log("Connected to the database");
+            budgetModel.find({})
+                    .then((data) => {
+                        res.json(data);
+                        mongoose.connection.close();
+                    })
+                    .catch((connectionError) => {
+                        console.log(connectionError);
+                    })
+        })
+        .catch((connectionError) => {
+            console.log(connectionError);
+        })
 });
 
-let expenesChart = document.getElementById('expenseChart').getContext('2d');
+app.put("/addEntry", (req, res) => {
+  mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+            console.log("Connected to the database");
+            newData = {
+                title: req.body.title,
+                price: req.body.price,
+                color: req.body.color
+            }
+            budgetModel.insertMany(newData)
+                    .then((data) => {
+                        console.log(data);
+                        res.json("Success");
+                        mongoose.connection.close();
+                    })
+                    .catch((connectionError) => {
+                        console.log(connectionError);
+                        res.send("Error");
+                    })
+        })
+        .catch((connectionError) => {
+            console.log(connectionError);
+            res.send("Error");
+        })
+})
 
-Chart.defaults.global.defaultFontColor = '#111';
-Chart.defaults.global.defaultFontFamily = "'Lora', sans-serif";
+app.get("/budget", (req, res) => {
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+          .then(() => {
+              console.log("Connected to the database");
+              budgetModel.find({})
+                      .then((data) => {
+                          res.json(data);
+                          mongoose.connection.close();
+                      })
+                      .catch((connectionError) => {
+                          console.log(connectionError);
+                      })
+          })
+          .catch((connectionError) => {
+              console.log(connectionError);
+          })
+  });
 
-let expenseChart = new Chart(expenesChart, {
-    type: 'pie',
-    data: {
-        labels: ['Food', 'Housing costs', 'Transportation', 'Telecommunication', 'Hygiene', 'Travel', 'Education', 'Books'],
-        datasets: [{
-            label: 'Expanses',
-            data: [340.00, 650.00, 23.50, 23.65, 13.20, 120.00, 152.00, 54.00],
-            backgroundColor: [
-                'rgb(230, 0, 0)',
-				'rgb(255, 102, 0)',
-                'rgb(204, 0, 153)',
-                'rgb(255, 204, 0)',
-				
-				'rgb(255, 204, 204)',
-				'rgb(153, 102, 255)',
-                'rgb(255, 0, 102)',
-                'rgb(153, 51, 51)'
-            ],
-            borderColor: [
-                'rgb(204, 0, 0)',
-                'rgb(230, 92, 0)',
-                'rgb(179, 0, 179)',
-                'rgb(230, 184, 0)',
-				
-				'rgb(255, 179, 179)',
-				'rgb(136, 77, 255)',
-                'rgb(230, 0, 92)',
-                'rgb(134, 45, 45)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        title:{
-			display: true,
-			text: 'Summary of your expanses by category',
-			fontSize: 18,
-			fontStyle: 'normal'
-			
-		},
-		legend:{
-			display: true,
-			position: 'right',
-		},
-		layout:{
-			padding:{
-				top: 30
-			}
-		},
-    }
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
 });
+
